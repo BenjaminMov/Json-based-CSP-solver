804a562456edeea42708db7893cded02b75318bb
BenjaminMov <82237771+BenjaminMov@users.noreply.github.com>
2022-05-30 18:26:22 -0700

    Add files via upload

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -6,47 +6,39 @@
 
 using namespace std;
 
-class JsonParser {
-    public:
+void init() {
+    ifstream file("problem.json");
+    reader.parse(file, jsonData);
+}
 
-        void init() {
-            ifstream file("problem.json");
-            reader.parse(file, jsonData);
-        }
+string getVar(int idx) {
+    return jsonData["Variables"][idx].asString();
+}
 
-        string getVar(int idx) {
-            return jsonData["Variables"][idx].asString();
-        }
+string ConstraintFn(int idx) {
+    return jsonData["Constraints"][idx]["function"].asString();
+}
 
-        string ConstraintFn(int idx) {
-            return jsonData["Constraints"][idx]["function"].asString();
-        }
+string ConstraintFirst(int idx) {
+    return jsonData["Constraints"][idx]["elem1"].asString();
+}
 
-        string ConstraintFirst(int idx) {
-            return jsonData["Constraints"][idx]["elem1"].asString();
-        }
+string ConstraintSecond(int idx) {
+    return jsonData["Constraints"][idx]["elem2"].asString();
+}
 
-        string ConstraintSecond(int idx) {
-            return jsonData["Constraints"][idx]["elem2"].asString();
-        }
+int min() {
+    return jsonData["Domain"]["min"].asInt();
+}
 
-        int min() {
-            return jsonData["Domain"]["min"].asInt();
-        }
+int max() {
+    return jsonData["Domain"]["max"].asInt();
+}
 
-        int max() {
-            return jsonData["Domain"]["max"].asInt();
-        }
+int varSize() {
+    return jsonData["Variables"].size();
+}
 
-        int varSize() {
-            return jsonData["Variables"].size();
-        }
-
-        int constraintSize() {
-            return jsonData["Constraints"].size();
-        } 
-
-    private:
-        Json::Value jsonData;
-        Json::Reader reader;
-};
+int constraintSize() {
+    return jsonData["Constraints"].size();
+}
